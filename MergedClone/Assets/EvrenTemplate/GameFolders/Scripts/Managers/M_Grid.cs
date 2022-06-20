9326a956970182b16e2e9a3d90f4c3c9ed3be399
DESKTOP-CJLODPR\Evren <EvrenOzgur1993@gmail.com>
2022-06-20 17:26:06 +0300

    Grid ve DiceSpawner Oluşturuldu.

--- MergedClone/Assets/EvrenTemplate/GameFolders/Scripts/Managers/M_Grid.cs
+++ MergedClone/Assets/EvrenTemplate/GameFolders/Scripts/Managers/M_Grid.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class M_Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GridItem GridItemPrefab;
 
-    // Update is called once per frame
-    void Update()
+    [HideInInspector] public GridItem[,] GridArray;
+
+    public int GridLenghtI = 5;
+    public int GridLenghtJ = 5;
+    private void Awake()
     {
-        
+        GridCreate();
+    }
+    private void OnEnable()
+    {
+        M_Observer.OnGameCreate += GameCreate;
+    }
+    private void OnDisable()
+    {
+        M_Observer.OnGameCreate -= GameCreate;
+
+    }
+    void GameCreate()
+    {
+       
+    }
+    void GridCreate()
+    {
+        GridArray = new GridItem[GridLenghtI , GridLenghtJ];
+        for (int i = 0; i < GridLenghtI; i++)
+        {
+            for (int j = 0; j < GridLenghtJ; j++)
+            {
+                GridItem _gridItem = Instantiate(GridItemPrefab , transform);
+                _gridItem.transform.position = new Vector3(i,j,0);
+                _gridItem.IndexI = i;
+                _gridItem.IndexJ = j;
+                GridArray[i, j] = _gridItem;
+            }
+        }
     }
 }
--- MergedClone/Assets/EvrenTemplate/GameFolders/Scripts/Managers/M_Spawner.cs
+++ MergedClone/Assets/EvrenTemplate/GameFolders/Scripts/Managers/M_Spawner.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class M_Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Dice[] DicePrefabs;
+    public Transform DiceContainer;
+    public Dice CurrentDice1;
+    public Dice CurrentDice2;
+    
+     public int SpawnDiceCount = 2;
+     public int InstantiateDiceNumber = 6;
+    [HideInInspector] public bool IsOneDice;
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        M_Observer.OnGameStart += GameStart;
+    }
+    private void OnDisable()
+    {
+        M_Observer.OnGameStart -= GameStart;
+
+    }
+    void GameStart()
+    {
+        SpawnDice();
+    }
+    void SpawnDice()
+    {
+        int a = 0;
+        int b = 0;
+        int _random = Random.Range(0, SpawnDiceCount);
+        print(_random);
+        if ( _random == 0)
+        {
+            int _diceNumber = Random.Range(1, InstantiateDiceNumber);
+            Dice _dice = Instantiate(DicePrefabs[_diceNumber], DiceContainer);
+            _dice.transform.localPosition = Vector3.zero;
+            CurrentDice1 = _dice;
+            IsOneDice = true;
+        }
+        else
+        {
+            while (a>=b)
+            {
+                a = Random.Range(1,InstantiateDiceNumber);
+                b = Random.Range(1,InstantiateDiceNumber);
+            }
+            Dice _dice1 = Instantiate(DicePrefabs[a], DiceContainer);
+            _dice1.transform.localPosition = new Vector3(0.5f, 0, 0);
+            CurrentDice1 = _dice1;
+            Dice _dice2 = Instantiate(DicePrefabs[b] , DiceContainer);
+            _dice2.transform.localPosition = new Vector3(-0.5f,0,0);
+            CurrentDice2 = _dice2;
+            IsOneDice = false;
+        }
     }
 }
